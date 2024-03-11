c4cc9d8f4f4f7cc3027d23920b2f6ec030031898
prieloize <159199824+prieloize@users.noreply.github.com>
2024-03-11 16:08:36 -0300

    Add files via upload

---
+++ teste_Python_Priscila/teste.py
@@ -0,0 +1,10 @@
+#importação das funções relacionadas ao time 
+import time
+
+nome = input("Digite seu nome: ")
+
+print("Olá", nome, "! Bem vindo ao Python!", sep=" " )
+print("Olá de novo, " + nome + "! Bem vindo!!!")
+
+#tempo de execução do programa
+time.sleep(10)
